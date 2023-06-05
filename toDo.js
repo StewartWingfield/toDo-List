291aa0b89041200d7a51fc288e3676a03f1ae358
Stewart Wingfield <sww915@gmail.com>
2023-06-04 19:17:13 -0500

    more js data

--- toDo.js
+++ toDo.js
@@ -1,36 +1,31 @@
-let arrayOfTodos = [
-    
-]
+let arrayOfTodos = []
 
 
  const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
-     .then( (response) => response.json())
-     .then( (json) => arrayOfTodos = json)
+     .then(res => res.json())
+     .then(json => console.log(json))
+     .then(data => arrayOfTodos = data)
+     console.log(arrayOfTodos)
  }
- console.log(arrayOfTodos)
 
-
-
-
-const logFetchTodos = () => console.log(fetchTodos());
 
 const logTodos = (arr) => {
     fetchTodos()
-    .then(res => res.json())
-    .then(json => console.log(json))
-    for (i = 0;  i < arr.length; i++){
-        console.log("todo for user " + arr[i]);
+    for (i = 0;  i < arr; i++){
+    console.log("todo for user " + arrayOfTodos[i].arr);
+    }
 }
-}
+
 logTodos(userId)
 logTodos(id)
 logTodos(title)
 logTodos(completed)
 
+
 const populateTodos = () => {
     fetchTodos()
     .then(res => res.json())
-    .then(todos => {
+    .then(todos => { 
     })
 }
