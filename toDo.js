7ab8b42e18880c5a5f83dbb3b308b5308f69c667
Stewart Wingfield <sww915@gmail.com>
2023-06-06 13:08:09 -0500

    add filter, clear, completed and not completed buttons

--- toDo.js
+++ toDo.js
@@ -1,5 +1,7 @@
-let listItems;
+/* let listItems; */
 let arrayOfTodos;
+let filteredData = [];
+let completedStatus = null;
 
 const fetchTodos = () => {
   fetch("https://jsonplaceholder.typicode.com/todos")
@@ -13,7 +15,46 @@ const logTodos = () => {
   console.log(arrayOfTodos);
 };
 
-const populateTodos = () => {
+
+function filterData() {
+  const userId = document.getElementById('userIdInput').value;
+
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(json => {
+      filteredData = json.filter(item => item.userId == userId);
+
+      if (completedStatus !== null) {
+        filteredData = filteredData.filter(item => item.completed === completedStatus);
+      }
+
+      displayData();
+    });
+}
+
+function clearData() {
+  filteredData = [];
+  completedStatus = null;
+  displayData();
+}
+
+function filterCompleted(status) {
+  completedStatus = status;
+  filterData();
+}
+
+function displayData() {
+  const dataList = document.getElementById('dataList');
+  dataList.innerHTML = '';
+
+  filteredData.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = item.title;
+    dataList.appendChild(li);
+  });
+}
+
+/* const populateTodos = () => {
   if (arrayOfTodos) {
     let orderedList = document.getElementById("todo-list");
     arrayOfTodos.forEach((todo) => {
@@ -23,4 +64,4 @@ const populateTodos = () => {
       orderedList.appendChild(listItems);
     });
   }
-};
+}; */
